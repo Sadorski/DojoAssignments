e16d8a90caf2aca7282ed224a461777d7a93cf23
Sultan Sultani <sadorski@Sultans-MBP.lan>
2018-02-26 20:13:15 -0800

    started JavaScript

---
+++ WebFundamentals/Javascript/Assignments/idodeclare.js
@@ -0,0 +1,21 @@
+var num1 = 23
+var num2 = 42
+var num3 = -34
+var num4 = -55
+var str1 = "Lord Huron"
+var str2 = "The Night We Met"
+var str3 = "Song absolutely kills me"
+var imhavingfun = true
+var imlying = false
+var undefine = undefined
+
+console.log("Michael Jordan's jersey number:", num1);
+console.log("The answer:", num2);
+console.log("They wanted a negative number for some reason:", num3);
+console.log("Why not another:", num4);
+console.log("that one artist with the sad song:", str1);
+console.log("that one sad song they sing:", str2);
+console.log("how does this song make you feel:", str3);
+console.log("Having so much fun:", imhavingfun);
+console.log("You kinda sound like you're lying", imlying);
+console.log("are you sure?:", undefine)
